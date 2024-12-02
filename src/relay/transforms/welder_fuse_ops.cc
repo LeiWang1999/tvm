f3b8845113dd08c6162fea8e4df5d5b0d2aa5012
Yang Wang <wyatuestc@gmail.com>
2024-12-02 12:16:57 +0000

    fix

--- src/relay/transforms/welder_fuse_ops.cc
+++ src/relay/transforms/welder_fuse_ops.cc
@@ -175,7 +175,7 @@ struct TaggedNodeGraph {
         fusible &= !(top_node->kind_ == kOutEWiseFusable && tnode->kind_ > kBroadcast);
         fusible &= (!tnode->reverse_inlined_);
       }
-      if (tnode->op_type_ == "transpose" || top_node->op_type_ == "transpose") fusible = false;
+      // if (tnode->op_type_ == "transpose" || top_node->op_type_ == "transpose") fusible = false;
       
       // add to group
       if (fusible) {
